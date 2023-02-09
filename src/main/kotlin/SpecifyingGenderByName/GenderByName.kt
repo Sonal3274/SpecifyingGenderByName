9896d37038c12c1809fc4fb46da98b40f8abbb23
Sonal Dhage <sonal.dhage@thermondo.de>
2023-02-09 13:46:23 +0100

    added file writing feature and new gender type

--- src/main/kotlin/SpecifyingGenderByName/GenderByName.kt
+++ src/main/kotlin/SpecifyingGenderByName/GenderByName.kt
@@ -1,58 +1,34 @@
-package RecognizingGenderByName
-
-import java.io.BufferedReader
-import java.io.FileReader
+import java.io.File
+import java.util.Scanner
 
 fun main() {
-    genderByName()
+    val scanner = Scanner(System.`in`)
+
+    println("Enter your gender (Male/Female/Transgender):")
+    val genderInput = scanner.nextLine()
+
+    var gender = when (genderInput.toLowerCase()) {
+        "male" -> Gender.MALE
+        "female" -> Gender.FEMALE
+        "transgender" -> Gender.TRANSGENDER
+        else -> {
+            println("Invalid input.")
+            return
+        }
+    }
+
+        //println("Your gender is $gender")
+        println("Enter your name:")
+        val nameInput = scanner.nextLine()
+
+        val file = when (gender) {
+            Gender.MALE -> File("src/main/kotlin/SpecifyingGenderByName/male.txt")
+            Gender.FEMALE-> File("src/main/kotlin/SpecifyingGenderByName/female.txt")
+            Gender.TRANSGENDER-> File("src/main/kotlin/SpecifyingGenderByName/trans.txt")
+        }
+
+        file.appendText("$nameInput\n")
 }
 
-fun genderByName() {
-    val list1OfStrings: MutableList<String> = ArrayList()
-    val list2OfStrings: MutableList<String> = ArrayList()
+enum class Gender { MALE, FEMALE, TRANSGENDER }
 
-    // load data from file
-    val bf1 = BufferedReader(
-        FileReader("src/main/kotlin/SpecifyingGenderByName/femaleNames.txt")
-    )
-
-    val bf2 = BufferedReader(
-        FileReader("src/main/kotlin/SpecifyingGenderByName/femaleNames.txt")
-    )
-    var line1 = bf1.readLine()
-    var line2 = bf2.readLine()
-
-    while (line1 != null) {
-        list1OfStrings.add(line1)
-        line1 = bf1.readLine()
-    }
-    while (line2 != null) {
-        list2OfStrings.add(line2)
-        line2 = bf2.readLine()
-    }
-    list1OfStrings.add("")
-    // closing bufferreader object
-    bf1.close()
-
-    // storing the data in arraylist to array
-    val array1 = list1OfStrings.toTypedArray()
-
-    list2OfStrings.add("")
-    // closing bufferreader object
-    bf2.close()
-
-    // storing the data in arraylist to array
-    val array2 = list2OfStrings.toTypedArray()
-
-    print("Enter a name: ")
-    var input = readLine()
-
-    for (i in array1) {
-        if (input == i)
-            println("$input is a female name")
-    }
-    for (i in array2) {
-        if (input == i)
-            println("$input is a male name")
-    }
-}
