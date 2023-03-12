e559dbcfc031254e5c0f9f0f87c45b66fb162884
Sonal Dhage <sonal.dhage@thermondo.de>
2023-03-12 19:51:23 +0100

    Updated code to write the new username in the particular file

--- src/main/kotlin/SpecifyingGenderByName/GenderByName.kt
+++ src/main/kotlin/SpecifyingGenderByName/GenderByName.kt
@@ -37,26 +37,66 @@ fun main() {
         println("Hello $nameInput, You are already registered!!!")
     } else {
         println("Hello $nameInput, you are not registered.")
-        println("Kindly register your name:")
+        println("Please register your name:")
 
-        // Create a new File object with the file path and name
-        val file = File("src/main/kotlin/SpecifyingGenderByName/transNames.txt")
-        
-        // Open the file for writing using a PrintWriter object
-       // val writer = PrintWriter(file)
-        val writer = BufferedWriter(FileWriter(file, true))
+        println("Enter your gender (Male/Female/Transgender):")
+        val newUserGender = scanner.nextLine()
 
-        // Write some Kotlin code to the file
-        val newUserName = scanner.nextLine()
-        writer.newLine()
-        writer.write(newUserName)
+        val genderNew = when (newUserGender.toLowerCase()) {
+            "male" -> Gender.MALE
+            "female" -> Gender.FEMALE
+            "transgender" -> Gender.TRANSGENDER
+            else -> {
+                println("Invalid input.")
+                return
+            }
+        }
 
-        // Close the PrintWriter object to save the changes to the file
-        writer.close()
+        println("Enter your name:")
+        if(genderNew == Gender.MALE){
+            // Create a new File object with the file path and name
+            val file = File("src/main/kotlin/SpecifyingGenderByName/maleNames.txt")
 
-        // Print a message to the console to confirm that the file was written successfully
-        println("Congratulations!!! You are successfully registered! : ${file.absolutePath}")
+            // Open the file for writing using a PrintWriter object
+            // val writer = PrintWriter(file)
+            val writer = BufferedWriter(FileWriter(file, true))
 
+            // Write some Kotlin code to the file
+            val newUserName = scanner.nextLine()
+            writer.newLine()
+            writer.write(newUserName)
+
+            // Close the PrintWriter object to save the changes to the file
+            writer.close()
+
+            // Print a message to the console to confirm that the file was written successfully
+            println("Congratulations!!! You are successfully registered! : ${file.absolutePath}")
+        } else if (genderNew == Gender.FEMALE){
+
+            val file = File("src/main/kotlin/SpecifyingGenderByName/femaleNames.txt")
+
+            val writer = BufferedWriter(FileWriter(file, true))
+
+            val newUserName = scanner.nextLine()
+            writer.write(newUserName)
+
+            writer.close()
+
+            println("Congratulations!!! You are successfully registered! : ${file.absolutePath}")
+        } else {
+
+            val file = File("src/main/kotlin/SpecifyingGenderByName/transNames.txt")
+
+            val writer = BufferedWriter(FileWriter(file, true))
+
+            val newUserName = scanner.nextLine()
+            writer.newLine()
+            writer.write(newUserName)
+
+            writer.close()
+
+            println("Congratulations!!! You are successfully registered! : ${file.absolutePath}")
+        }
     }
 }
 
